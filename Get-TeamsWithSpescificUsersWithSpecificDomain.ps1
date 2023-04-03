72c225fc5a43e5f6967ae467ac88be5b37dfb57b
Tony Bardalen <51480132+Bangdalen@users.noreply.github.com>
2023-04-03 10:36:23 +0200

    Update Get-TeamsWithSpescificUsersWithSpecificDomain.ps1
    Some cleanup

--- Get-TeamsWithSpescificUsersWithSpecificDomain.ps1
+++ Get-TeamsWithSpescificUsersWithSpecificDomain.ps1
@@ -7,14 +7,18 @@ Import-Module -Name MicrosoftTeams
 #Connect to Microsoft Teams
 Connect-MicrosoftTeams
 
-#Every Team with every member:
+#List out every team with every member
 $AllTeams = Get-Team
+
+#Loop through every Team and sort out users
 Foreach ($team in $AllTeams) 
 {
 	$teamUsers = Get-TeamUser -groupid $team.groupid
 	
+	#Finds users with the specified domain
 	$domainUsers = $teamUsers | Where-Object { $_.User.EndsWith($domain) }
 	
+	#If found, list out every team with users from that domain
 	if ($domainUsers.Count -gt 0) {
 		Write-Host -NoNewLine "Users from "
 		Write-Host -NoNewLine -BackgroundColor "Darkblue" -ForegroundColor "White" $domain
@@ -22,3 +26,6 @@ Foreach ($team in $AllTeams)
 		Write-Host -BackgroundColor "Green" -ForegroundColor "Black" $team.DisplayName
 	}
 }
+
+#Disconnect
+Disconnect-MicrosoftTeams
